f84151ec062e237afaa1471ec3333fb6a8b0fea8
unknown <sadi.prajapati@techwagger.com>
2025-12-09 15:33:47 +0530

    latest

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -6,7 +6,7 @@ import jobRoleReducer from "./slices/jobRoleSlice"
 const rootReducer = combineReducers({
   user: userReducer,
   auth: authReducer,
-    jobRoles: jobRoleReducer,
+  jobRoles: jobRoleReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
